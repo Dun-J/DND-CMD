c4021f91d26bea5bc7b7e76fe2ce9bce97fcc50e
Dun_J <90296332+Dun-J@users.noreply.github.com>
2025-01-30 13:32:25 -0700

    More File Set Up

--- armor.h
+++ armor.h
@@ -1,3 +1,9 @@
 #ifndef ARMOR_H
 #define ARMOR_H
+#include "item.h"
+    struct Armor: public Item{
+        public:
+            char type;
+            int mod;
+    };
 #endif
--- character.h
+++ character.h
@@ -2,16 +2,29 @@
 #define CHARACTER_H
 #include "item.h"
 #include <vector>
+#include <string>
 class Character{
     public:
+        std::string name;
         bool addXP(int);
         bool addLevel(int);
         bool addLevel();
-        bool addItem();
+        bool addItem(Item);
+        Item findItem(std::string);
     private:
         bool _useMilestone;
         int _xp;
         int _level;
+        std::vector<std::string> languages;
         std::vector<Item> _itemList;
+        std::string _class;
+        int _str;
+        int _dex;
+        int _con;
+        int _wis;
+        int _int;
+        int _cha;
+        int _hp;
+        int _tempHp;
 };
 #endif
