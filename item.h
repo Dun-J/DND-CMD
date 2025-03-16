7e09ae1794b4ea76bd82362a45223189a17b3a8d
Dun_J <90296332+Dun-J@users.noreply.github.com>
2025-03-16 17:06:25 -0600

    Item.h Magic Item Property Bare Bones

--- item.h
+++ item.h
@@ -5,6 +5,8 @@ struct Item{
 public:
 std::string name;
 std::string description;
+// Properties Should Be Two Chars -- St De Co In Wi Ch -- for a mod to stat -- +n for addition to hit and atk
+std::string properties;
 private:
 };
 #endif
